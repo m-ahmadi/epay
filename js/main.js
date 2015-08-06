98e4569a824e102848f60f7d60ceffb72bdcd840
m-ahmadi <mohammad.ahmadi1989@yahoo.com>
2015-08-06 12:18:08 +0430

    next

---
+++ js/main.js
@@ -0,0 +1,46 @@
+$(function () {
+	$('.smbutton').on('mouseenter', function(){
+		//$('.sub-menu').addClass('visible');
+		// $('.sub-menu').animate(
+			// { width: '400px', height: '495px', opacity: 1, zIndex: 1 },
+			// {
+				// duration : 200,
+				// specialEasing: {},
+				// complete: function () {
+					// // execute when finished
+					// // this = element before .animate
+				// }
+			// }
+		// );
+		
+		if (!$('.sub-menu').is(':animated')) {
+			$('.sub-menu').slideDown({duration: 200})
+			.css({zIndex: 1});
+		}
+		
+		
+		
+	});
+	$('.sub-menu, .menu').on('mouseleave', function(){
+		//$('.sub-menu').removeClass('visible');
+		
+		// $('.sub-menu').animate(
+			// { width: '0px', height: '0px', opacity: 0, zIndex: 0 },
+			// {
+				// duration : 200,
+				// specialEasing: {},
+				// complete: function () {
+					// // execute when finished
+					// // this = element before .animate
+				// }
+			// }
+		// );
+		
+		if (!$('.sub-menu').is(':animated')) {
+			$('.sub-menu').slideUp(200);
+		}
+		
+		
+	});
+	
+});
